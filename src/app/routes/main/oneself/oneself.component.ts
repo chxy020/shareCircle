06b7c90588cc76645bda1a67fe6c113ac3637626
chenxy <chenxy@tingyun.com>
2020-09-19 01:23:56 +0800

    oneself

--- src/app/routes/main/oneself/oneself.component.ts
+++ src/app/routes/main/oneself/oneself.component.ts
@@ -10,19 +10,16 @@ import { HttpService } from 'src/app/shared/services/http';
 
 export class OneSelfComponent implements OnInit {
 	
-	// data = [];
 	loading = true;
-	// me:any;
 
-	// page = 0;
-	// limit = 10;
-
-	// headImg = "./assets/images/headimg.png";
-	// videoImg = "./assets/images/listimg.jpg";
+	headImg = "./assets/images/headimg.png";
 
 	pageType = 1;
 
 	baseUrl = "";
+	uid;
+
+	detail = {};
 
 	constructor(
 		private route: ActivatedRoute,
@@ -35,10 +32,34 @@ export class OneSelfComponent implements OnInit {
 	ngOnInit() {
 		// this.id = +this.route.snapshot.data.id;
 		// this.title = this.titles[this.id];
+		this.uid = window['context']['uid'];
+		this.baseUrl = window["context"]["apiroot"];
+
+		this.getImgOrName();
 	}
 
 	changePage(i):void{
         this.pageType = i;
 	}
 	
+	getImgOrName():void{
+		this.loading = true;
+
+		const params: Map<string, any> = new Map<string, any>();
+		params.set("uid",this.uid);
+
+		let url = "/jqkj/circle/getImgOrName";
+		this.http.get(url, params, null).subscribe(data => {
+			// console.log(data)
+			if(data.status == 0){
+				this.detail = data.data || {};
+
+				// this.getFirstComment();
+			}
+			this.loading = false;
+		}, error => {
+			console.error(error);
+			this.loading = false;
+		});
+	}
 }
