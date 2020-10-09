d0a9e78a7391be7621e18698ebaa062449b91ef2
chenxy <chenxy@tingyun.com>
2020-10-10 00:03:59 +0800

    微信分享

--- src/app/routes/details/details.component.ts
+++ src/app/routes/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, Injector } from '@angular/cor
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpService } from 'src/app/shared/services/http';
 import { Location } from '@angular/common';
+declare var wx:any;
 
 @Component({
 	selector: 'video-details',
@@ -25,6 +26,8 @@ export class DetailsComponent implements OnInit {
 
 	tabType = 1;
 	commentMsg = "";
+
+	sharedPop = false;
 
 	get getVideoPath(): string {
 		return this.baseUrl + this.detail.video_path;
@@ -55,6 +58,7 @@ export class DetailsComponent implements OnInit {
 
 
 		this.findUserPublish();
+		this.getWxSign();
 	}
 
 	// meetClick(item):void{
@@ -171,4 +175,93 @@ export class DetailsComponent implements OnInit {
 	}
 
 	
+
+	getWxSign(){
+		//获取随机串
+		let createNonceStr = function() {
+		    return Math.random().toString(36).substr(2, 15);
+		};
+		    
+		// timestamp
+		let createTimeStamp = function () {
+		    return new Date().getTime() / 1000 + '';
+		};
+
+		this.loading = true;
+		const params: Map<string, any> = new Map<string, any>();
+		params.set("url",location.href.split('#')[0]);
+		params.set("timestamp",createTimeStamp());
+		params.set("nonce",createNonceStr());
+		this.http.post("/jqkj/wxShare/getSignature", params, null).subscribe(data => {
+			// console.log(wx);
+			this.setWxSign(data);
+			this.loading = false;
+		}, error => {
+			console.error(error);
+			this.loading = false;
+		});
+	}
+	
+	setWxSign(data:any){
+		wx.config({
+			debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+			appId: "wx04ad9813254366a7", // 必填，公众号的唯一标识
+			timestamp: data.timestamp , // 必填，生成签名的时间戳
+			nonceStr: data.nonce, // 必填，生成签名的随机串
+			signature: data.signature,// 必填，签名，见附录1
+			jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage","onMenuShareQQ"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+	}
+
+	sharedAppMessage(){
+		wx.ready(function(){
+			console.log("wx----ready---")
+			var sdata = {
+				imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+				link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+				title: "测试微信分享", // 分享标题
+				desc: "分享描述", // 分享描述
+				success: function () {
+					console.log("分享成功");
+				},
+				cancel: function () {
+					console.log("分享失败");
+				}
+				
+			};
+			wx.updateAppMessageShareData(sdata);// 发送给朋友
+			// wx.updateTimelineShareData(sdata);//分享到朋友圈
+		});
+		wx.error(function(res){
+			// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+			alert("抱歉，服务器初始化错误。");
+			console.log(res);
+		});
+	}
+
+	sharedTimeline(){
+		wx.ready(function(){
+			console.log("wx----ready---")
+			var sdata = {
+				imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+				link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+				title: "测试微信分享", // 分享标题
+				desc: "分享描述", // 分享描述
+				success: function () {
+					console.log("分享成功");
+				},
+				cancel: function () {
+					console.log("分享失败");
+				}
+				
+			};
+			// wx.updateAppMessageShareData(sdata);// 发送给朋友
+			wx.updateTimelineShareData(sdata);//分享到朋友圈
+		});
+		wx.error(function(res){
+			// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+			alert("抱歉，服务器初始化错误。");
+			console.log(res);
+		});
+	}
 }
--- src/app/routes/invitecode/invitecode.component.ts
+++ src/app/routes/invitecode/invitecode.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Injector, Pipe, PipeTransform } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpService } from 'src/app/shared/services/http';
-
+declare var wx:any;
 
 @Component({
 	selector: 'invitecode',
@@ -19,6 +19,8 @@ export class InviteCodeComponent implements OnInit {
 	uid;
 
 	sharedCode = "";
+
+	sharedPop = false;
 
 	constructor(
 		private route: ActivatedRoute,
@@ -41,7 +43,177 @@ export class InviteCodeComponent implements OnInit {
 		}else{
 			this.sharedCode = "";
 		}
+
+		this.getWxSign();
 	}
 
+	getWxSign(){
+		//获取随机串
+		let createNonceStr = function() {
+		    return Math.random().toString(36).substr(2, 15);
+		};
+		    
+		// timestamp
+		let createTimeStamp = function () {
+		    return new Date().getTime() / 1000 + '';
+		};
+
+		// 	data: {
+		// 		url:location.href.split('#')[0], //url 如果写的是固定的值的话，分享之后在分享会报错
+		// 		timestamp: createTimeStamp,
+		// 		nonce: createNonceStr
+		// 	},
+
+		this.loading = true;
+		const params: Map<string, any> = new Map<string, any>();
+		params.set("url",location.href.split('#')[0]);
+		params.set("timestamp",createTimeStamp());
+		params.set("nonce",createNonceStr());
+		this.http.post("/jqkj/wxShare/getSignature", params, null).subscribe(data => {
+			// console.log(wx);
+			this.setWxSign(data);
+			this.loading = false;
+		}, error => {
+			console.error(error);
+			this.loading = false;
+		});
+		
+		// $.ajax({
+		// 	url:"http://39.107.249.187:8082/jqkj/wxShare/getSignature",
+		// 	type:"post",
+		// 	data: {
+		// 		url:location.href.split('#')[0], //url 如果写的是固定的值的话，分享之后在分享会报错
+		// 		timestamp: createTimeStamp,
+		// 		nonce: createNonceStr
+		// 	},
+		// 	timeout: 10000, //超时时间设置为10秒；
+		// 	success: function(data) {
+		// 		console.log(data);
+		// 		//微信初始化
+		// 		wx.config({
+		// 		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		// 		    appId: "wx04ad9813254366a7", // 必填，公众号的唯一标识
+		// 		    timestamp: data.timestamp , // 必填，生成签名的时间戳
+		// 		    nonceStr: data.nonce, // 必填，生成签名的随机串
+		// 		    signature: data.signature,// 必填，签名，见附录1
+		// 		    jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage","onMenuShareQQ"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		// 		});
+		// 		wx.ready(function(){
+		// 		    var sdata = {
+		// 		 		imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+		// 		 		link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+		// 		    	title: "测试微信分享", // 分享标题
+		// 		 		desc: "分享描述", // 分享描述
+		// 			    success: function () {
+		// 					console.log("分享成功");
+		// 			    },
+		// 			    cancel: function () {
+		// 					console.log("分享失败");
+		// 			    }
+					    
+		// 			};
+		// 		    wx.updateAppMessageShareData(sdata);// 发送给朋友
+		// 		    wx.updateTimelineShareData(sdata);//分享到朋友圈
+		// 		});
+		// 		wx.error(function(res){
+		// 		    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+		// 			alert("抱歉，服务器初始化错误。");
+		// 			console.log(res);
+		// 		});
+		// 	},
+		// 	error:function(xhr, type, errorThrown){
+		// 		alert("error");
+		// 		//异常处理；
+		// 		alert(xhr);
+		// 		alert(type);
+		// 		alert(errorThrown);
+		// 	}
+		// })
+	}
 	
+	setWxSign(data:any){
+		wx.config({
+			debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+			appId: "wx04ad9813254366a7", // 必填，公众号的唯一标识
+			timestamp: data.timestamp , // 必填，生成签名的时间戳
+			nonceStr: data.nonce, // 必填，生成签名的随机串
+			signature: data.signature,// 必填，签名，见附录1
+			jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage","onMenuShareQQ"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+		// wx.ready(function(){
+		// 	console.log("wx----ready---")
+		// 	var sdata = {
+		// 		imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+		// 		link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+		// 		title: "测试微信分享", // 分享标题
+		// 		desc: "分享描述", // 分享描述
+		// 		success: function () {
+		// 			console.log("分享成功");
+		// 		},
+		// 		cancel: function () {
+		// 			console.log("分享失败");
+		// 		}
+				
+		// 	};
+		// 	// wx.updateAppMessageShareData(sdata);// 发送给朋友
+		// 	// wx.updateTimelineShareData(sdata);//分享到朋友圈
+		// });
+		// wx.error(function(res){
+		// 	// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+		// 	alert("抱歉，服务器初始化错误。");
+		// 	console.log(res);
+		// });
+	}
+
+	sharedAppMessage(){
+		wx.ready(function(){
+			console.log("wx----ready---")
+			var sdata = {
+				imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+				link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+				title: "测试微信分享", // 分享标题
+				desc: "分享描述", // 分享描述
+				success: function () {
+					console.log("分享成功");
+				},
+				cancel: function () {
+					console.log("分享失败");
+				}
+				
+			};
+			wx.updateAppMessageShareData(sdata);// 发送给朋友
+			// wx.updateTimelineShareData(sdata);//分享到朋友圈
+		});
+		wx.error(function(res){
+			// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+			alert("抱歉，服务器初始化错误。");
+			console.log(res);
+		});
+	}
+
+	sharedTimeline(){
+		wx.ready(function(){
+			console.log("wx----ready---")
+			var sdata = {
+				imgUrl: "http://39.107.249.187:8082/jqkj/fileupload/video/3b3e353052a24476aed3d6b0dc41e572.jpg", // 分享图标
+				link: "http://39.107.249.187:8082/jqkj/cricle/getSelectedCircle",
+				title: "测试微信分享", // 分享标题
+				desc: "分享描述", // 分享描述
+				success: function () {
+					console.log("分享成功");
+				},
+				cancel: function () {
+					console.log("分享失败");
+				}
+				
+			};
+			// wx.updateAppMessageShareData(sdata);// 发送给朋友
+			wx.updateTimelineShareData(sdata);//分享到朋友圈
+		});
+		wx.error(function(res){
+			// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+			alert("抱歉，服务器初始化错误。");
+			console.log(res);
+		});
+	}
 }
