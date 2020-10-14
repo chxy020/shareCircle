596f684c6f48a2ba9e0605d5e7bc910e68b6f81f
duhong-hub <147443966@qq.com>
2020-10-14 18:10:47 +0800

    Merge branch 'master' of https://github.com/chxy020/shareCircle

--- src/app/layout/console/header/titleheader.component.ts
+++ src/app/layout/console/header/titleheader.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { SubjectService } from 'src/app/shared/services/subjectService.service';
+declare var circle:any;
 
 @Component({
     selector: 'title-header',
@@ -16,7 +17,7 @@ import { SubjectService } from 'src/app/shared/services/subjectService.service';
                 <li (click)="shareCode()" >分享邀请码</li>
             </ul>
             <ul *ngIf="title=='作者主页'" >
-                <li (click)="shareCode()" >发布分享</li>
+                <li (click)="choiceShare()" >发布分享</li>
                 <li (click)="quitCircle()" >退出圈子</li>
             </ul>
         </div>
@@ -71,6 +72,41 @@ export class TitleHeaderComponent implements OnInit {
     
     shareCode(){
         this.router.navigate(['/invitecode']);
+    }
+
+    choiceShare(){
+        let u = navigator.userAgent;
+        let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+        let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        if (isAndroid) {
+            //这个是安卓操作系统
+            // alert("android");
+            if(typeof circle != "undefined"){
+                circle.choiceJoinOrShareCircle();
+                // var msg = H5JsStorage.showDetail("{'test':'123'}");
+                // document.getElementById("native").innerHTML = "native响应：" + msg;
+            }else{
+                alert("circle不存在");
+            }
+        }
+        if (isIOS) {
+            //这个是ios操作系统
+            // alert("ios");
+            // if(typeof webkit != "undefined"){
+            //     if(typeof webkit.messageHandlers != "undefined"){
+            //         if(typeof webkit.messageHandlers.showDetail != "undefined"){
+            //             var msg = webkit.messageHandlers.showDetail.postMessage("{'test':'123'}");
+            //             document.getElementById("native").innerHTML = "native响应：" + msg;
+            //         }else{
+            //             alert("window.webkit.messageHandlers.showDetail不存在");
+            //         }
+            //     }else{
+            //         alert("window.webkit.messageHandlers不存在");
+            //     }
+            // }else{
+            //     alert("window.webkit不存在");
+            // }
+        }
     }
 
     quitCircle(){
