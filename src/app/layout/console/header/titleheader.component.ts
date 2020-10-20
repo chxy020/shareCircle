884ff130689d76a6e237510e1fe8beb9d40288c5
chenxy <chenxy@tingyun.com>
2020-10-20 23:22:07 +0800

    download

--- src/app/layout/console/header/titleheader.component.ts
+++ src/app/layout/console/header/titleheader.component.ts
@@ -11,7 +11,7 @@ import { SubjectService } from 'src/app/shared/services/subjectService.service';
         <div (click)="back()" class="topDiv-btnL"></div>
         {{title}}
 
-        <div (click)="menuBtn($event,menuele)" *ngIf="title=='我的主页' || title=='作者主页' || title == '关于圈子'" class="topDiv-btnR"></div>
+        <div (click)="menuBtn($event,menuele)" *ngIf="title=='我的主页' || title=='作者主页'" class="topDiv-btnR"></div>
         <div #menuele style="display:none;" class="tip_operlist">
             <ul *ngIf="title=='我的主页'" >
                 <li (click)="shareCode()" >分享邀请码</li>
@@ -30,6 +30,8 @@ import { SubjectService } from 'src/app/shared/services/subjectService.service';
 export class TitleHeaderComponent implements OnInit {
     
     title;
+    //是否本地跳转，本地跳转，关闭webview
+    n = 0;
 
     constructor(
         private route: ActivatedRoute,
@@ -43,11 +45,62 @@ export class TitleHeaderComponent implements OnInit {
     ngOnInit() {
         this.route.data.subscribe(params => {
 			this.title = params["title"] || "";
+        });
+        
+        this.route.queryParams.subscribe(params => {
+			this.n = params["n"];
 		});
     }
 
     back(){
-		this.location.back();
+        if(this.n){
+            this.closeWebView();
+        }else{
+            this.location.back();
+        }
+    }
+    
+    closeWebView(){
+		let u = navigator.userAgent;
+        let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+        let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+
+		if (isAndroid) {
+            //这个是安卓操作系统
+            // alert("android");
+            // alert(window["circle"]);
+            if(typeof window["circle"] != "undefined"){
+                // alert(window["circle"].publishCircle)
+                try{
+                    window["circle"].closeWebview();
+                }catch(ex){
+                    alert("closeWebview catch")
+                }
+                // var msg = circle.choiceJoinOrShareCircle();
+                // var msg = H5JsStorage.showDetail("{'test':'123'}");
+                // document.getElementById("native").innerHTML = "native响应：" + msg;
+            }else{
+                alert("circle不存在");
+            }
+        }
+        if (isIOS) {
+            //这个是ios操作系统
+            // alert("ios");
+            // if(typeof webkit != "undefined"){
+            //     if(typeof webkit.messageHandlers != "undefined"){
+            //         if(typeof webkit.messageHandlers.showDetail != "undefined"){
+            //             var msg = webkit.messageHandlers.showDetail.postMessage("{'test':'123'}");
+            //             document.getElementById("native").innerHTML = "native响应：" + msg;
+            //         }else{
+            //             alert("window.webkit.messageHandlers.showDetail不存在");
+            //         }
+            //     }else{
+            //         alert("window.webkit.messageHandlers不存在");
+            //     }
+            // }else{
+            //     alert("window.webkit不存在");
+            // }
+        }
     }
     
     currentItem;
--- src/app/routes/shared/download/download.component.ts
+++ src/app/routes/shared/download/download.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from 'src/app/shared/services/http';
 
 @Component({
 	selector: 'download-app',
@@ -7,23 +8,49 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class DownloadComponent implements OnInit {
-	sharedCode;
-
+	loading = false;
 	downloadUrl;
+	baseUrl;
+	isWx = false;
 	constructor(
+		private http: HttpService,
 		private route: ActivatedRoute,
 		private router: Router
 	) {
 		this.downloadUrl =  window["context"]["downloadUrl"];
-
+		this.baseUrl = window["context"]["apiroot"];
 		if(!this.isWeiXin()){
-			window.location.href = this.downloadUrl;
+			this.isWx = false;
+			this.getApkUrl();
+			// window.location.href = this.downloadUrl;
+		}else{
+			this.isWx = true;
 		}
 	}
 
 	ngOnInit() {
 		// this.sharedCode = this.route.snapshot.paramMap.get('code');
 		//如果在微信端，显示提示，如果不是微信，直接跳转下载链接
+	}
+
+	getApkUrl():void{
+		this.loading = true;
+
+		const params: Map<string, any> = new Map<string, any>();
+		params.set("type",0);
+
+		let url = "/jqkj/user/appNow";
+		this.http.get(url, params, null).subscribe(data => {
+			// console.log(data)
+			if(data.status == 0){
+				let obj = data.data || {};
+				window.location.href = this.baseUrl + obj.path;
+			}
+			this.loading = false;
+		}, error => {
+			console.error(error);
+			this.loading = false;
+		});
 	}
 
 	//判断是否为微信浏览器
